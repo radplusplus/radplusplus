1e4474273568a2720079c39b01d9d074b309d776
René Mailloux <rene.mailloux@radplusplus.com>
2017-04-06 16:07:24 -0400

    correction de bug fixture

--- radplusplus/fixtures/custom_scripts/Stock Entry.js
+++ radplusplus/fixtures/custom_scripts/Stock Entry.js
@@ -6,7 +6,7 @@
 /////////////////////////////// Handles ///////////////////////////////
 
 frappe.ui.form.on("Stock Entry",{
-	refresh: function(frm) {
+	"refresh": function(frm) {
 		// 2016-10-17 - JDLP
 		// Lancer la fonction "LoadAttributesValues" au "onLoad" du formulaire parent.
 		LoadAttributesValues(false, frm, "items")
@@ -14,19 +14,19 @@ frappe.ui.form.on("Stock Entry",{
 });
 
 frappe.ui.form.on("Stock Entry Detail",{
-	item_code: function(frm, cdt, cdn) {
+	"item_code": function(frm, cdt, cdn) {
 		// 2016-09-17 - JDLP
 		// Lancer la fonction "ShowHideAttributes" quand l'item_code change.
 		ShowHideAttributes(false, frm, cdt, cdn, true, false)
 	},
 	
-	create_variant: function(frm, cdt, cdn) {
+	"create_variant": function(frm, cdt, cdn) {
 		// 2016-10-17 - JDLP
 		// Lancer la fonction "CreateItemVariant"lorque le bouton "create_variant" est active.
 		CreateItemVariant(false, frm, cdt, cdn, true, true)
 	}
 
-	reconfigure: function(doc, cdt, cdn) {
+	"reconfigure": function(doc, cdt, cdn) {
 		// 2016-11-01 - JDLP
 		// Lancer la fonction "ReconfigurerItemVariant" lorque le bouton "reconfigure" est active.
 		ReconfigurerItemVariant(false, doc, cdt, cdn)
