839a37ee8b4ca2921774e285d585b9da3b6a638d
René Mailloux <rene.mailloux@radplusplus.com>
2017-04-06 10:33:50 -0400

    Ajout du custom script BOM.js en fixture.

---
+++ radplusplus/fixtures/custom_scripts/bom.js
@@ -0,0 +1,42 @@
+// Copyright (c) 2015, Frappe Technologies Pvt. Ltd. and Contributors
+// License: GNU General Public License v3. See license.txt
+
+/////////////////////////////// Handles ///////////////////////////////
+
+frappe.ui.form.on("BOM Operation",{
+	"operation": function(frm, cdt, cdn) {	
+		var d = locals[cdt][cdn];
+
+		if(!d.operation) return;
+
+		frappe.call({
+			"method": "frappe.client.get",
+			args: {
+				doctype: "Operation",
+				name: d.operation
+			},
+			callback: function (data) {
+				if(data.message.description) {
+					frappe.model.set_value(d.doctype, d.name, "description", data.message.description);
+				}
+				if(data.message.workstation) {
+					frappe.model.set_value(d.doctype, d.name, "workstation", data.message.workstation);
+				}
+				frappe.model.set_value(d.doctype, d.name, "no_oper", d.idx *10);			
+				//msgprint("d : " + d.idx);
+			}
+		})
+	}
+});
+
+frappe.ui.form.on("BOM Item",{
+	"item_code": function(frm, cdt, cdn) {	
+		var d = locals[cdt][cdn];
+
+		if(!d.item_code) return;
+
+		frappe.model.set_value(d.doctype, d.name, "no_mtl", d.idx *10);
+	}
+});
+
+
